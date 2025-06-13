0ea30aa9d905b6895411cf1e5ce9466e319d4429
vishnu-mukati <mukativishnu1232@gmaila.com>
2025-06-13 23:05:10 +0530

    adding the multiple select prop to the component

--- src/assets/components/MuiSelect.tsx
+++ src/assets/components/MuiSelect.tsx
@@ -2,20 +2,29 @@ import { Box, TextField, MenuItem } from '@mui/material';
 import { useState } from 'react';
 const MuiSelect = () => {
 
-    const [country,setCountry] = useState('');
-
+    const [countries,setCountries] = useState<string[]>([]);
+    console.log(countries);
+    
     const handleClick = (event : React.ChangeEvent<HTMLInputElement>) =>{
-        setCountry(event.target.value as string);
+        const value = event.target.value;
+        setCountries(typeof value === 'string' ? value.split(',') : value);
     }
 
     return (
-        <Box width='130px'>
+        <Box width='250px'>
             <TextField 
-            value = {country} 
+            value = {countries} 
             label='Select Country'
             select
             fullWidth
-            onChange={handleClick}>
+            size = 'small'
+            color='secondary'
+            helperText='Please select your country'
+            onChange={handleClick}
+            SelectProps = {{
+                multiple : true,
+            }}
+            >
                 <MenuItem value = 'IND'>INDIA</MenuItem>
                 <MenuItem value='US'>USA</MenuItem>
                 <MenuItem value='AU'>Australia</MenuItem>
