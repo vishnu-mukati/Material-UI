c1f65e44a5f604c6a267ca977d0fd1dd20027836
vishnu-mukati <mukativishnu1232@gmaila.com>
2025-06-13 23:58:02 +0530

    Adding the RadioButton component with the another rapper component like formcontrol formlebal etc with prop of them

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import './App.css'
 // import MuiTypography from './assets/components/MuiTypography'
 // import MuiButton from './assets/components/MuiButton';
 // import MuiTextField from './assets/components/MuiTextField'
-import MuiSelect from './assets/components/MuiSelect';
+// import MuiSelect from './assets/components/MuiSelect';
+import MuiRadioButton from './assets/components/MuiRadioButton';
 function App() {
   return (
     <div className='App'>
       {/* <MuiTypography/> */}
       {/* <MuiButton/> */}
       {/* <MuiTextField/> */}
-      <MuiSelect/>
+      {/* <MuiSelect/> */}
+      <MuiRadioButton/>
     </div>
   )
 }
