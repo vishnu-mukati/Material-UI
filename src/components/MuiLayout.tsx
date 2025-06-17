50147684233aa62491ce00bbf0535e6ab8985e3f
vishnu-mukati <mukativishnu1232@gmaila.com>
2025-06-17 19:41:19 +0530

    Adding Stack component in the layout

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,32 +1,36 @@
-import { Box } from "@mui/material";
-import { blue } from "@mui/material/colors";
+import { Box, Stack, Divider } from "@mui/material";
 
 const MuiLayout = () => {
-    return (
-        <>
-            <Box
-                sx={{
-                    backgroundColor: "primary.main",
-                    color: "white",
-                    height: "100px",
-                    width: "100px",
-                    padding:'16px',
-                    "&:hover": {
-                        backgroundColor: "primary.light",
-                    },
-                }}
-            >
-                CodeVolution
-            </Box>
-            <Box
-                display="flex"
-                height="100px"
-                width="100px"
-                bgcolor="success.light"
-                p={2}
-            ></Box>
-        </>
-    );
+  return (
+    <Stack
+      sx={{ border: "1px solid" }}
+      direction="row"
+      spacing={2}
+      divider={<Divider orientation="vertical" flexItem />}
+    >
+      <Box
+        sx={{
+          backgroundColor: "primary.main",
+          color: "white",
+          height: "100px",
+          width: "100px",
+          padding: "16px",
+          "&:hover": {
+            backgroundColor: "primary.light",
+          },
+        }}
+      >
+        CodeVolution
+      </Box>
+      <Box
+        display="flex"
+        height="100px"
+        width="100px"
+        bgcolor="success.light"
+        p={2}
+      ></Box>
+    </Stack>
+  );
 };
 
 export default MuiLayout;
